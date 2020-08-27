33d71e36d52effce3dc9761f2d4f9297011442c7
peralseb28 <68518701+peralseb28@users.noreply.github.com>
2020-08-27 18:17:10 -0300

    Player y animaciones
    Se cambió la cápsula por el asset
    Se crea script para controlar animaciones
    Se aumentó un poco la iluminación para ver mejor al player

--- Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/Scipts/PlayerController.cs
+++ Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/Scipts/PlayerController.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float playerSpeed = 5;
+    public float playerSpeed ;
+    private Animator playerAnimator;
+    private float vertical; 
+    
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         Movement();
+        Animation();
     }
 
     private void Movement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal"); //X
-        float vertical = Input.GetAxisRaw("Vertical"); //Z
-        Vector3 playerMovement = new Vector3(horizontal, 0, vertical) * playerSpeed * Time.deltaTime;
+        vertical = Input.GetAxisRaw("Vertical"); // Z
+        Vector3 playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
     }
+
+    private void Animation()
+    {
+        if (vertical != 0 && Input.GetKey(KeyCode.LeftShift) == false)
+        {
+           
+            playerAnimator.SetBool("OnIdle", false);
+            playerAnimator.SetBool("Walk", true);
+            playerAnimator.SetBool("Run", false);
+            playerSpeed = 2;
+            Vector3 playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
+            transform.Translate(playerMovement * playerSpeed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.LeftShift) == true)
+        {
+            
+            playerAnimator.SetBool("OnIdle", false);
+            playerAnimator.SetBool("Walk", false);
+            playerAnimator.SetBool("Run", true);
+            playerSpeed = 5;
+            Vector3 playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
+            transform.Translate(playerMovement * playerSpeed * Time.deltaTime);
+        }
+        else
+        {
+            playerAnimator.SetBool("OnIdle", true);
+            playerAnimator.SetBool("Walk", false);
+            playerAnimator.SetBool("Run", false);
+        }
+        
+    }
+    
 }
