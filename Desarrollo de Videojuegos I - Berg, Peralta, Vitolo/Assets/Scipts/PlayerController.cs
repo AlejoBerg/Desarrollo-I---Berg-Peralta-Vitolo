98dea4df731530c5c5fe23baafefa9b526d1ed8f
peralseb28 <68518701+peralseb28@users.noreply.github.com>
2020-08-31 14:46:19 -0300

    Animaciones / Input
    Se agrega movimiento Der/Izq
    Se agrega animaciones para el mov de izq a der
    Se agrega la animacion del salto
    Se corrige la acción de correr al tocar shift y no estar avanzando

--- Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/Scipts/PlayerController.cs
+++ Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/Scipts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float playerSpeed ;
     private Animator playerAnimator;
-    private float vertical; 
+    private float vertical;
+    private float horizontal;
+    private Vector3 playerMovement;
     
     void Start()
     {
@@ -22,39 +24,112 @@ public class PlayerController : MonoBehaviour
     private void Movement()
     {
         vertical = Input.GetAxisRaw("Vertical"); // Z
-        Vector3 playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
+        horizontal = Input.GetAxisRaw("Horizontal");
+        playerMovement = new Vector3(horizontal, 0f, vertical) * playerSpeed * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
+
+    
+        Debug.Log("horizontal" + horizontal);
+       
+        
     }
 
     private void Animation()
     {
+      
+       
         if (vertical != 0 && Input.GetKey(KeyCode.LeftShift) == false)
         {
            
             playerAnimator.SetBool("OnIdle", false);
             playerAnimator.SetBool("Walk", true);
             playerAnimator.SetBool("Run", false);
+            playerAnimator.SetBool("Jump", false);
+            playerAnimator.SetBool("Rigth", false);
+            playerAnimator.SetBool("Left", false);
             playerSpeed = 2;
-            Vector3 playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
+            playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
             transform.Translate(playerMovement * playerSpeed * Time.deltaTime);
+          
+            if (Input.GetKey(KeyCode.Space) == true)
+            {
+                playerAnimator.SetBool("Jump", true);
+                playerAnimator.SetBool("Walk", false);
+                playerSpeed = 2;
+                playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
+                transform.Translate(playerMovement * playerSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space) == true)
+            {
+                playerAnimator.SetBool("Jump", false);
+                playerAnimator.SetBool("Walk", true);
+            }
         }
-        else if(Input.GetKey(KeyCode.LeftShift) == true)
+        else if(Input.GetKey(KeyCode.LeftShift) == true && vertical != 0 )
         {
-            
             playerAnimator.SetBool("OnIdle", false);
             playerAnimator.SetBool("Walk", false);
             playerAnimator.SetBool("Run", true);
+            playerAnimator.SetBool("Jump", false);
+            playerAnimator.SetBool("Rigth", false);
+            playerAnimator.SetBool("Left", false);
             playerSpeed = 5;
-            Vector3 playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
+            playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
             transform.Translate(playerMovement * playerSpeed * Time.deltaTime);
+            
+            
+            if (Input.GetKey(KeyCode.Space) == true)
+            {
+                playerAnimator.SetBool("Jump", true);
+                playerAnimator.SetBool("Run", false);
+                playerSpeed = 5;
+                playerMovement = new Vector3(0, 0, vertical) * playerSpeed * Time.deltaTime;
+                transform.Translate(playerMovement * playerSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space) == true)
+            {
+                playerAnimator.SetBool("Jump", false);
+                playerAnimator.SetBool("Run", true);
+            }
+        }
+        else if (Input.GetKey(KeyCode.Space) == true && vertical == 0  && Input.GetKey(KeyCode.LeftShift) == false)
+        {
+            playerAnimator.SetBool("Jump", true);
+            playerAnimator.SetBool("OnIdle", false);
+            playerAnimator.SetBool("Run", false);
+            playerAnimator.SetBool("Walk", false);
+            playerAnimator.SetBool("Rigth", false);
+            playerAnimator.SetBool("Left", false);
+        }
+        else if (horizontal == 1 && vertical == 0 && Input.GetKey(KeyCode.D))
+        {
+            playerAnimator.SetBool("Rigth", true);
+            playerAnimator.SetBool("Left", false);
+            playerAnimator.SetBool("OnIdle", false);
+            playerAnimator.SetBool("Walk", false);
+            playerAnimator.SetBool("Run", false);
+            playerAnimator.SetBool("Jump", false);
+        }
+        
+        else if (horizontal == -1  && vertical == 0 && Input.GetKey(KeyCode.A))
+        {
+            playerAnimator.SetBool("Rigth", false);
+            playerAnimator.SetBool("Left", true);
+            playerAnimator.SetBool("Walk", false);
+            playerAnimator.SetBool("Run", false);
+            playerAnimator.SetBool("Jump", false);
+            playerAnimator.SetBool("OnIdle", false);
         }
         else
         {
             playerAnimator.SetBool("OnIdle", true);
             playerAnimator.SetBool("Walk", false);
             playerAnimator.SetBool("Run", false);
+            playerAnimator.SetBool("Jump", false);
+            playerAnimator.SetBool("Rigth", false);
+            playerAnimator.SetBool("Left", false);
         }
-        
     }
-    
 }
