5d4e151a98d60b2c8a0759fb7d182b6435eae25c
Alejo B <alejo.110601@gmail.com>
2020-09-25 18:44:48 -0300

    Agregado de nuevas arañas

---
+++ Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/_Assets/Spider2D/SpiderBlinking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class SpiderBlinking : MonoBehaviour
+{
+    private VideoPlayer myVideoPlayer;
+    private float randomStartTime = 0;
+
+    private void Start()
+    {
+        myVideoPlayer = GetComponent<VideoPlayer>();
+        randomStartTime = Random.Range(1, 7);
+        StartCoroutine("DelayBeforeStartVideo");
+    }
+
+    IEnumerator DelayBeforeStartVideo()
+    {
+        yield return new WaitForSeconds(randomStartTime);
+        myVideoPlayer.Play();
+    }
+}
