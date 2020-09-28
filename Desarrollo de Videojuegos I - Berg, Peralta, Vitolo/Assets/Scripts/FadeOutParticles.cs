7d4fad8ad7c59aff2b3ba6bf9b54e207e4c8fef9
Alejo B <alejo.110601@gmail.com>
2020-09-28 16:00:50 -0300

    Agregados Visuales
    - Cree pared de particulas para el collider del puzzle
    - Rehice las arañas
    - Coloque arañas
    - Comence a trabajar en el fade out de las luciernagas

---
+++ Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/Scripts/FadeOutParticles.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutParticles : MonoBehaviour
+{
+    [SerializeField] private float fadeOutTime;
+    [SerializeField] private float particlesAmount;
+    private ParticleSystem myParticles;
+    private float counter = 0;
+
+
+
+    private void Start()
+    {
+        myParticles = GetComponent<ParticleSystem>();
+        StartCoroutine("FadeParticle");
+    }
+
+    private IEnumerator FadeParticle()
+    {
+        counter = 0;
+        var emission = myParticles.emission;
+
+        while (counter < fadeOutTime)
+        {
+            counter += Time.deltaTime;
+            emission.rateOverTime = particlesAmount;
+            particlesAmount -= Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
