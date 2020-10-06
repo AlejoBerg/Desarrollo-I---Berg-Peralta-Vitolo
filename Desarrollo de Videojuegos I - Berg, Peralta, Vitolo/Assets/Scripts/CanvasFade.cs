b633503d87219bd6ec88d0723ee7dc3d36f510fa
Alejo B <alejo.110601@gmail.com>
2020-10-06 11:47:17 -0300

    Nuevos agregados cueva
    - Fade para paso de nivel
    - Reposicionamiento de piedras de la cueva
    - Aplique cambios de prefab player y antorcha.

---
+++ Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/Scripts/CanvasFade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasFade : MonoBehaviour
+{
+    [SerializeField] private float duration = 1;
+    [SerializeField] private int startAlpha = 0;
+    [SerializeField] private int endAlpha = 1;
+    private CanvasGroup canvasGroupRef;
+
+    private void Start()
+    {
+        canvasGroupRef = GetComponent<CanvasGroup>();
+    }
+
+    public void DoFade()
+    {
+        StartCoroutine("FadeCanvasObject");
+    }
+
+    IEnumerator FadeCanvasObject() 
+    {
+        float counter = 0f;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            canvasGroupRef.alpha = Mathf.Lerp(startAlpha, endAlpha, counter / duration);
+
+            yield return null;
+        }
+    }
+}
---
+++ Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    [SerializeField] private string nextLevelName = "";
+    [SerializeField] private float delayBeforeLoadNextLevel = 0;
+    [SerializeField] private CanvasFade fadeOutImage = null;
+    [SerializeField] private AudioSource fadeOutMusic = null;
+    private float initialVolume = 0;
+
+    private void Start()
+    {
+        initialVolume = fadeOutMusic.volume;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            fadeOutImage.DoFade();
+            StartCoroutine("FadeMusicVolume");
+            StartCoroutine("DelayBeforeLoadNextLvl");
+        }
+    }
+
+    IEnumerator FadeMusicVolume()
+    {
+        float counter = 0f;
+
+        while (fadeOutMusic.volume > 0)
+        {
+            counter += Time.deltaTime;
+            fadeOutMusic.volume = Mathf.Lerp(initialVolume, 0, counter / delayBeforeLoadNextLevel);
+            yield return null;
+        }
+    }
+
+    IEnumerator DelayBeforeLoadNextLvl()
+    {
+        yield return new WaitForSeconds(delayBeforeLoadNextLevel);
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
