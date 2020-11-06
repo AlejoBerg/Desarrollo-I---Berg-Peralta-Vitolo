c9cfa3c08e2203bec8094182952cb1f023a95c51
Alejo B <alejo.110601@gmail.com>
2020-11-06 18:17:59 -0300

    Agregados lvl2
    + Pergaminos
    + Collider del rio
    + Textos y huds

---
+++ Desarrollo de Videojuegos I - Berg, Peralta, Vitolo/Assets/ShowImageWhenCollide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowImageWhenCollide : MonoBehaviour
+{
+    [SerializeField] private GameObject imageToShow = null;
+    [SerializeField] private float imageToShowTimer = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            imageToShow.SetActive(true);
+            StartCoroutine(HideImage());
+        }        
+    }
+
+    IEnumerator HideImage()
+    {
+        yield return new WaitForSeconds(imageToShowTimer);
+        imageToShow.SetActive(false);
+    }
+}
